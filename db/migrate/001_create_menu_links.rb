91e505051b31901071d8dc69a4de87dc43425a4a
Tide <tide@dies-world.com>
2010-04-12 13:55:37 -0400

    Changed the default vaulue for is_enabled to true.

--- db/migrate/001_create_menu_links.rb
+++ db/migrate/001_create_menu_links.rb
@@ -3,7 +3,7 @@ class CreateMenuLinks < ActiveRecord::Migration
     create_table :menu_links do |t|
       t.column "name", :string, :limit => 60, :null => false
       t.column "url", :string, :limit => 255, :null => false
-      t.column "is_enabled", :boolean, :default => false, :null => false
+      t.column "is_enabled", :boolean, :default => true, :null => false
       t.column "new_window", :boolean, :default => false, :null => false
       t.column "link_type", :integer, :default => 0
       t.column "position", :integer, :default => 1
