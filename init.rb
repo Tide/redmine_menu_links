c768bc30a00fe5cae57991158ac0f09f02ab77ec
Tide <tide@dies-world.com>
2010-04-12 18:38:52 -0400

    Changed icon to custom icon instead of stock icon.

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 require 'redmine'
+require 'admin_menu_hooks'
 
 RAILS_DEFAULT_LOGGER.info 'Menu Links plugin 0.0.1 for Redmine'
 
@@ -6,10 +7,10 @@ Redmine::Plugin.register :redmine_menu_links do
   name 'Menu Links Plugin'
   author 'Tide, Yuki Kita'
   description 'A plugin which adds links to the top menu of Redmine.'
-  url         'http://github.com/Tide/redmine_menu_links'
+  url         'http://github.com/Tide/redmine_menu_links' if respond_to?(:url)
   version '0.0.1'
     
-  menu :admin_menu, :menu_links, { :controller => 'menu_links', :action => 'index'}, :caption => :label_menu_links, :html => { :class => 'icon icon-attachment' }
+  menu :admin_menu, :menu_links, { :controller => 'menu_links', :action => 'index'}, :caption => :label_menu_links, :html => { :class => 'icon icon-menu-links' }
 end
 
 MenuLink.show if MenuLink.table_exists?
---
+++ lib/admin_menu_hooks.rb
@@ -0,0 +1,23 @@
+class AdminMenuHooks < Redmine::Hook::ViewListener
+  
+  # Add a question CSS class
+  def view_layouts_base_html_head(context = { })
+    o = <<CSS
+  <style type="text/css">
+
+.icon {
+background-position: 0% 50%;
+background-repeat: no-repeat;
+padding-left: 20px;
+padding-top: 2px;
+padding-bottom: 3px;
+}
+
+.icon-menu-links { background-image: url(../images/menu-links.png); }
+
+  </style>
+CSS
+    return o
+  end
+end
+
