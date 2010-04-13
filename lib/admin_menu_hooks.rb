156d9052e35a063559064a89e034272010c8920d
Tide <tide@dies-world.com>
2010-04-12 20:21:43 -0400

    Removed duplicate code.

--- lib/admin_menu_hooks.rb
+++ lib/admin_menu_hooks.rb
@@ -1,17 +1,8 @@
 class AdminMenuHooks < Redmine::Hook::ViewListener
   
-  # Add a question CSS class
   def view_layouts_base_html_head(context = { })
     o = <<CSS
   <style type="text/css">
-
-.icon {
-background-position: 0% 50%;
-background-repeat: no-repeat;
-padding-left: 20px;
-padding-top: 2px;
-padding-bottom: 3px;
-}
 
 .icon-menu-links { background-image: url(../plugin_assets/redmine_menu_links/images/menu-links.png); }
 
